e895f600738031d2615476194131a484a4265516
Beacon Mac mini <beacon@golfzon.com>
2025-07-15 16:20:24 +0700

    no message

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:baby_diary/main.dart';
+import 'package:women_diary/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const BabyDiary());
+    await tester.pumpWidget(const WomenDiary());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
