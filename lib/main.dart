85e1ed98046117155cce14b359ca0b9b153b4b35
Beacon Mac mini <beacon@golfzon.com>
2025-07-31 16:02:49 +0700

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:women_diary/common/firebase/firebase_option.dart';
 import 'package:women_diary/common/firebase/firebase_user.dart';
 import 'package:women_diary/database/local_storage_service.dart';
-import 'package:women_diary/l10n/app_localizations.dart';
-import 'package:women_diary/routes/route_name.dart';
-import 'package:women_diary/routes/routes.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
